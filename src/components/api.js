d752596c4a280171a4a15920d8587a77e7e821f5
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-05-12 11:27:06 -0300

    refactor: optimizing the use of parameters

--- src/components/api.js
+++ src/components/api.js
@@ -1,32 +1,21 @@
-const axios = require('axios')
+const axios = require('axios').default
+
+const api = axios.create({})
 
 module.exports = (endpoint, signer) => {
-  axios.defaults.baseURL = endpoint
+
+  api.defaults.baseURL = endpoint
+  api.defaults.headers = {
+    authorization: `Bearer ${signer.signJWT()}`
+  }
+
   return {
     post: async (route, payload = {}) => {
-      const { data } = await axios.post(
-        route,
-        signer.signMsg(payload),
-        {
-          headers: {
-            authorization: `Bearer ${signer.signJWT()}`
-          }
-        }
-      )
-
+      const { data } = await api.post(route, signer.signMsg(payload))
       return data
     },
     get: async (route, payload) => {
-      const { data } = await axios.get(
-        route,
-        {
-          params: payload,
-          headers: {
-            authorization: `Bearer ${signer.signJWT()}`
-          }
-        }
-      )
-
+      const { data } = await api.get(route, { params: payload })
       return data
     }
   }
