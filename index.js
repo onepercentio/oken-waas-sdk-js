2b9b56041901911fc8274221752b51d366e8e1a7
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-05-12 11:25:35 -0300

    refactor: add network and and update of parameter and up with address

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken')
 const wallet = require('./src/wallets')
 const contracts = require('./src/contracts')
 const endpoints = require('./src/config/endpoints')
+const networks = require('./src/config/networks')
 const okenSigner = require('./src/components/okenSigner')
 const API = require('./src/components/api')
 
 module.exports = {
-	connect: ({ network, okenClientId, privateKey }) => {
+	connect: ({ endpoint, network, address, okenClientId, privateKey }) => {
 		const signer = okenSigner(okenClientId, privateKey)
-		const api = API(network, signer)
+		const api = API(endpoint, signer)
 		return {
 			wallets: wallet(api),
-			contracts: contracts(api),
+			contracts: contracts(api, network, address),
 		}
 	},
-	networks: endpoints
+	networks,
+	endpoints
 }
--- src/contracts/index.js
+++ src/contracts/index.js
@@ -6,20 +6,28 @@ const abiMap = {
   'nft-control': 'ControlledNFTFactory'
 }
 
-const contract = (contractId, api) => {
-  const abi = JSON.parse(fs.readFileSync(`src/abis/${abiMap[contractId]}.json`)).abi
+const typesAndMutability = v => ({ type: v.type, mutability: v.stateMutability })
+
+const typeFunctions = v => v.type === 'function'
+
+const buildApiUrls = (contractBaseUrl, api) => (v, method) => v.mutability === 'view'
+  ? (payload) => api.get(`${contractBaseUrl}/${method}`, payload)
+  : (payload) => api.post(`${contractBaseUrl}/${method}`, { params: payload })
+
+const contract = (contractId, api, network, address) => {
+  const { abi } = JSON.parse(fs.readFileSync(`src/abis/${abiMap[contractId]}.json`))
+
+  const contractApiServices = buildApiUrls(`/${network}/${contractId}/${address}`, api)
+
   return _(abi)
     .keyBy('name')
-    .mapValues(v => ({ type: v.type, mutability: v.stateMutability }))
-    .pickBy(v => v.type === 'function')
-    .mapValues((v, method) => v.mutability === 'view' ?
-      (payload, options) => api.get(`/contracts/${contractId}/${method}`, payload) :
-      (payload, options) => api.post(`contracts/${contractId}/${method}`, { params: payload })
-    )
+    .mapValues(typesAndMutability)
+    .pickBy(typeFunctions)
+    .mapValues(contractApiServices)
     .value()
 }
 
-module.exports = api => ({
-  nft: () => contract('nft', api),
+module.exports = (api, network, address) => ({
+  nft: () => contract('nft', api, network, address),
   // erc20: contract('erc20')
 })
