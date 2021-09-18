8d6e27f46df7cfe81ffc936521d7587570a0c713
Andre <andrefmeirelles@gmail.com>
2021-09-18 12:07:43 -0300

    added new networks

--- src/config/networks.js
+++ src/config/networks.js
@@ -2,10 +2,15 @@ module.exports = {
   ETHEREUM: {
     KOVAN: 'kovan',
     ROPSTEN: 'ropsten',
+    RINKEBY: 'rinkeby',
     MAINNET: 'mainnet'
   },
   CELO: {
     ALFAJORES: 'alfajores',
     FORNO: 'celo'
+  },
+  POLYGON: {
+    MUMBAI: 'mumbai',
+    MAINNET: 'polygon'
   }
 }
