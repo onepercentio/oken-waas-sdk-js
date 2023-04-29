59346477df22ca1fc0bc18c646879111e0e493e6
highlander <andrefmeirelles@gmail.com>
2023-04-29 18:23:56 -0300

    added initial exports declaration

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,6 @@
 export * from '../types/web3-v1-contracts'
+
+export declare function connect(arg: any): any;
+export declare const networks: any;
+export declare const endpoints: any;
+export declare const typedData: any;
