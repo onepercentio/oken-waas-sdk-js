3120a183a41f9511e797081062b00d4c5010d773
highlander <andrefmeirelles@gmail.com>
2023-07-04 09:32:10 -0300

    converted typed data

--- src/typed_data/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ERC1155_PERMIT: require('./ERC1155_Permit.json'),
-  ERC20_AUTHORIZE: require('./ERC20_Authorize.json'),
-}
---
+++ src/typed_data/index.ts
@@ -0,0 +1,7 @@
+import ERC1155_PERMIT from './ERC1155_Permit.json'
+import ERC20_AUTHORIZE from './ERC20_Authorize.json'
+
+export default {
+  ERC1155_PERMIT,
+  ERC20_AUTHORIZE
+}
